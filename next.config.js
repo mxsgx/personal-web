78e084b608ba10596288391a908e36c7ad580181
masga <mxsgx.id@gmail.com>
2023-11-19 13:01:04 +0700

    Contact me redirection

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/contact-me",
+        destination: process.env.CONTACT_ME_REDIRECT_DEST,
+        permanent: false
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
